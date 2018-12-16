f698866ed5fca26551afefe8734bd317c111a43c
Mario Linz <gelight@gmx.de>
2018-12-16 23:56:19 +0100

    Prepared application for first implementations.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="justshowme">
+    <router-link to="/">Home</router-link>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  * { box-sizing: border-box; margin: 0; padding: 0; }
+  html, body { width: 100%; height: 100%; }
+  body {
+    background: #111;
+    background-image: url(https://wallpaperlayer.com/img/2015/7/dark-geometric-wallpaper-761-845-hd-wallpapers.jpg);
+    background-size: cover;
+    color: #999;
   }
-}
+
+  #justshowme {
+
+  }
 </style>
